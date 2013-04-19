f94e9c43a8a8490ffe9d666dc7f3f4bc16cc7b29
Priscila Carvalho <pri.carvalho86@gmail.com>
2013-04-18 22:39:43 -0300

    Melhoria na legibilidade e ajustes na logica do conversor

--- conversor_de_encoding.sh
+++ conversor_de_encoding.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
+#
+# Only for UNIX Operating Systems
+#
+# For the destination directory (DIRETORIO_DESTINO), is important within the folder 'alterados', 
+# is made a copy of the folders/files from the source directory (DIRETORIO_ORIGEM).
+#
+# Has no problem that the files to be converted also be copied, because the conversion, 
+# it is 'replace' the files that are on the destination folder.
+#
+# Files listed in the operation of IF condition are not valid files to perform the conversion 
+# to UTF-8, in other words, can not be converted to this type.(necessary to experiment with other types of encoding)
 
-DIR_RAIZ="web/"
-	
-for DIR_ORIGEM in $(find $DIR_RAIZ -type f ) #CAMINHO E NOME DO ARQUIVO
+DIRETORIO_RAIZ="web/"
+
+for DIRETORIO_ORIGEM in $(find $DIRETORIO_RAIZ -type f ) #ENCONTRA O CAMINHO E NOME DE CADA ARQUIVO DE ORIGEM
 
 	do
 
-		ENCODING="$(file -b --mime-encoding $DIR_ORIGEM)" #ENCODE DO ARQUIVO
-		#ENCODING="$(file -I $DIR_ORIGEM)" #FUNCIONA NO MAC
+		ENCODING_ARQUIVO_ORIGEM="$(file -b --mime-encoding $DIRETORIO_ORIGEM)"
+		
+		if [ "$ENCODING_ARQUIVO_ORIGEM" != "utf-8" ]  && [ "$ENCODING_ARQUIVO_ORIGEM" != "us-ascii" ] && [ "$ENCODING_ARQUIVO_ORIGEM" != "binary" ];
 
-		DIR_DESTINO="alterados/$DIR_ORIGEM"
-	
-		if [ "$ENCODING" != "utf-8" ];
-			
 				then
-				
-					#echo "$(iconv -s -f $ENCODING -t UTF-8 $DIR_ORIGEM > $DIR_DESTINO)"
-					echo "ORIGEM: $DIR_ORIGEM - $ENCODING" #EXIBE O CAMINHO, NOME E ENCODING DO ARQUIVO ORIGEM
-					echo "DESTINO: $DIR_DESTINO - $ENCODING" #EXIBE O CAMINHO, NOME E ENCODING DO ARQUIVO DESTINO
+					
+					DIRETORIO_DESTINO="alterados/$DIRETORIO_ORIGEM"
+					
+					echo "$(iconv -s -f $ENCODING_ARQUIVO_ORIGEM -t UTF-8 $DIRETORIO_ORIGEM > $DIRETORIO_DESTINO)" # EXECUTA A CONVERSAO
+					
+					ENCODING_ARQUIVO_DESTINO="$(file -b --mime-encoding $DIRETORIO_DESTINO)"
+					
+					echo "ORIGEM: $DIRETORIO_ORIGEM - $ENCODING_ARQUIVO_ORIGEM" #EXIBE O CAMINHO, NOME E ENCODING DO ARQUIVO ORIGEM
+					echo "DESTINO: $DIRETORIO_DESTINO - $ENCODING_ARQUIVO_DESTINO" #EXIBE O CAMINHO, NOME E ENCODING DO ARQUIVO DESTINO
 		fi
 done
+
+# PT-BR
+#
+# Somente para sistemas operacionais baseados em UNIX
+#
+# Para o diretorio de destino (DIRETORIO_DESTINO), eh importante que dentro da pasta 'alterados', 
+# seja feita uma copia das pastas/arquivos do diretorio origem (DIRETORIO_ORIGEM). 
+#   
+# Nao tem problema que os arquivos a serem convertidos tambem sejam copiados, pois na conversao, 
+# ele faz 'replace' dos arquivos que estao na pasta de destino.
+#
+# Os arquivos relacionados na operacao de condicao IF nao sao arquivos validos para efetuar
+# a conversao para UTF-8, ou seja, nao podem ser convertidos para este tipo. (necessario testar com outros tipos de encoding)
